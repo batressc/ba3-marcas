4c0dfb2d59b9243b90602014a0dec8d910439537
Luis Gustavo Fernández Batres <elclon_fernanbrans@hotmail.com>
2016-10-20 23:05:04 -0600

    importscripts + webworker.d.ts

--- app/shared/workers/echo.worker.ts
+++ app/shared/workers/echo.worker.ts
@@ -1,22 +1,27 @@
-//let that: Worker;
+/// <reference path="../../../typings/globals/webworkers/lib.webworker.d.ts" />
 
-let interval: NodeJS.Timer;
+if('function' === typeof importScripts) {
+    importScripts(
+        '../../../node_modules/systemjs/dist/system.src.js',
+        '../../../systemjs.config.js');
 
-self.addEventListener('message', (ev) => {
-    switch (ev.data) {
-        case 'stop':
-            if (interval) {
-                clearInterval(interval);
-                interval = null;
-            }
-            break;
-        case 'begin':
-            if (!interval) {
-                interval  = setInterval(() => {
-                    self.postMessage(Date.now().toString());
-                }, 100);
-            }
-            break;
-    }
-}, false);
+    let interval: NodeJS.Timer;
 
+    self.addEventListener('message', (ev) => {
+        switch (ev.data) {
+            case 'stop':
+                if (interval) {
+                    clearInterval(interval);
+                    interval = null;
+                }
+                break;
+            case 'begin':
+                if (!interval) {
+                    interval  = setInterval(() => {
+                        self.postMessage(Date.now().toString());
+                    }, 100);
+                }
+                break;
+        }
+    }, false);
+}
